9633a24d2371340ce6c6ab8a01ed9cadc718f9ec
esther-gan <gankahee@gmail.com>
2024-07-26 12:25:10 +0800

    add all build back

---
+++ build/assets/js/1fbf9216.ae4d8e92.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[122],{251:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/r2ata-web/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ build/assets/js/acecf23e.87625a98.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/r2ata-web/blog","blogTitle":"Blog"}')}}]);
